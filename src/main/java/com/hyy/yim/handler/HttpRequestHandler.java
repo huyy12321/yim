79a13a09222621b5abe02e656aeca963bcaba69a
hyy <577892771@qq.com>
2020-01-11 13:22:22 +0800

    搭建环境

--- src/main/java/com/hyy/yim/handler/HttpRequestHandler.java
+++ src/main/java/com/hyy/yim/handler/HttpRequestHandler.java
@@ -1,5 +1,6 @@
 package com.hyy.yim.handler;
 
+import com.hyy.yim.service.ChatServer;
 import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.ssl.SslHandler;
@@ -36,10 +37,25 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
 
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
-        if (wsUri.equalsIgnoreCase(request.uri())) {
-            // 增加引用计数，传递给下一个handler
-            // 之所以需要调用 retain()方法，是因为调用 channelRead()方法完成之后，它将调用 FullHttpRequest 对象上的 release()方法以释放它的资源。
-            ctx.fireChannelRead(request.retain());
+        if (request.uri().contains(wsUri)) {
+            String uri = request.uri();
+            int i = uri.indexOf("id=");
+            if(i == -1) {
+                ctx.write("id没传");
+                ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+                return;
+            }
+            String id = uri.substring(i + 3);
+            Channel channel = ChatServer.userMap.get(id);
+            if(channel != null) {
+                ctx.writeAndFlush("id已存在");
+                ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+            } else{
+                ChatServer.userMap.put(id,ctx.channel());
+                // 增加引用计数，传递给下一个handler
+                // 之所以需要调用 retain()方法，是因为调用 channelRead()方法完成之后，它将调用 FullHttpRequest 对象上的 release()方法以释放它的资源。
+                ctx.fireChannelRead(request.retain());
+            }
         } else {
             if(HttpHeaderUtil.is100ContinueExpected(request)) {
                 // 为了符合http 1.1规范
--- src/main/java/com/hyy/yim/handler/TextWebSocketFrameHandler.java
+++ src/main/java/com/hyy/yim/handler/TextWebSocketFrameHandler.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 
 /**
  * @author hyy
@@ -17,6 +18,31 @@ public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextW
 
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        // 往group中所有的客户端发消息，增加消息引用计数
+        // retain()方法的调用是必需的，因为当 messageReceived()方法返回时，TextWebSocketFrame
+        // 的引用计数将会被减少。由于所有的操作都是异步的，因此，writeAndFlush()方法可能会在
+        // messageReceived()方法返回之后完成，而且它绝对不能访问一个已经失效的引用。
 
+        group.writeAndFlush(msg.retain());
+    }
+
+    /**
+     * 重写userEventTriggered 通知所有所有已经连接的WebSocket客户端 新的客户端已经连接上了
+     * @param ctx
+     * @param evt
+     * @throws Exception
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if(evt == WebSocketServerProtocolHandler.ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
+            // 从 pipeline 中移除 HttpRequestHandler,之后将不会接收到http信息
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            group.writeAndFlush(new TextWebSocketFrame(
+                    "Client " + ctx.channel().id() + "joined"));
+            // 将新的WebSock channel 加入到group中，以便之后接收消息
+            group.add(ctx.channel());
+        } else{
+            super.userEventTriggered(ctx, evt);
+        }
     }
 }
